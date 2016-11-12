b7e26ea101a5864e5f302b9269645fb0527f12a6
JBS <mtihm@mbpjodyluijter2.nhb-gb.ibmmobiledemo.com>
2016-11-12 15:39:44 +0100

    Allow all hosts

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '_#&@+ov)y4fo!$puo4o17z)gi92txsi+xku1xkpmprutpo-z)#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
